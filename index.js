86d1b2835c7d3abd18b47a6422d79643c3ec9a70
i0 <niamnesh@gmail.com>
2020-04-04 21:30:29 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function aexec(cmd) {
+  const exec = require("child_process").exec;
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        console.warn(error);
+      }
+      resolve(stdout ? stdout : stderr);
+    });
+  });
+}
+
+module.exports = { default: aexec };
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Install
+
+```bash
+npm install aexec
+```
+
+# Usage
+
+```js
+import aexec from "../aexec";
+
+console.log(await aexec("ls"));
+/* Output:
+index.js
+package.json
+readme.md
+*/
+```
